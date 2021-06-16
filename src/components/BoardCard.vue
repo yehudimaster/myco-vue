f7e426c7bf80c455fc12653e48ab3bcd72c696c2
VoidConcept <carlos@estudioseijo.com>
2021-06-16 20:51:03 +0200

    feat: Board View Creation

---
+++ src/components/BoardCard.vue
@@ -0,0 +1,55 @@
+<template>
+  <router-link
+    class="board-card"
+    :to="{ name: 'Board', params: { name, id }}">
+     <span class="board-card-title">{{ name }}</span>
+  </router-link>
+</template>
+
+<script>
+export default {
+  name: 'board-card',
+  props: {
+    id: String,
+    name: String
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+  .board-card {
+    background-color: #546E7A;
+    color: #fafafa;
+    border: none;
+    box-sizing: border-box;
+    border-radius: 4px;
+    box-shadow: 0 0 0 0.5px rgba(49,49,93,.03),
+                0 2px 5px 0 rgba(49,49,93,.1),
+                0 1px 2px 0 rgba(0,0,0,.08);
+    text-decoration: none;
+    padding: 1rem;
+    margin: 0 0.5rem 1.5rem;
+    text-align:left;
+    transition: all 600ms ease;
+    width:100%;
+    height:100%;
+
+      &-title {
+        font-size: 1.2rem;
+      }
+
+      &:hover {
+        background-color: #90a4ae;
+        font-weight: bold;
+      }
+
+      @media screen and (min-width: 600px) {
+        width:33%;
+      }
+
+      @media screen and (min-width: 1024px) {
+        width:250px;
+      }
+  }
+</style>
---
+++ src/views/Board.vue
@@ -0,0 +1,87 @@
+<template>
+  <section>
+    <h3><span>Mis paneles</span> > {{ name }}</h3>
+    <input
+      type="text"
+      placeholder="Añade una lista"
+      v-model="listName"
+      @keyup.enter="add()"
+    />
+    <div class="container">
+      <column
+        v-for="(list, index) in boardList"
+        :key="index"
+        :listId="list.id"
+        :name="list.name">
+        </column>
+    </div>
+  </section>
+</template>
+
+<script>
+import Column from '@/components/Column'
+export default {
+  name: 'board-view',
+  components: { Column },
+  props: {
+    id: String,
+    name: String
+  },
+  data () {
+    return {
+      listName: '',
+      boardList: [
+        { id: 1, name: 'Todo' },
+        { id: 2, name: 'Doing' }
+      ]
+    }
+  },
+
+  methods: {
+    add () {
+      this.boardList.push({ name: this.listName })
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  section {
+    text-align:left;
+  }
+
+  h3 {
+    color:#37474f;
+    text-align:left;
+    margin:1.5rem;
+  }
+
+  span {
+    color:#546e7a;
+  }
+
+   input {
+    background-color:#607d8b;
+    color:#607d8b;
+    border:0;
+    border-radius: 3px;
+    box-shadow: 0 0 0 0.5px rgba(49,49,93,.03),
+                0 2px 5px 0 rgba(49,49,93,.1),
+                0 1px 2px 0 rgba(0,0,0,.08);
+    font-size:1.2rem;
+    outline:0;
+    margin: 0 1rem;
+    padding:1rem;
+    transition: all 600ms ease;
+
+      &:focus,
+      &:active {
+        background-color:#fafafa;
+      }
+
+      &::placeholder {
+        color:#fafafa;
+      }
+   }
+
+</style>
