675a1051a90123749fd46a5f83b7a38db517848c
VoidConcept <carlos@estudioseijo.com>
2021-06-19 09:44:11 +0200

    fix: correcciones en mutations-types y añadiendo actions y setters al store

--- src/store/index.js
+++ src/store/index.js
@@ -1,13 +1,15 @@
 import { createStore, createLogger } from 'vuex'
 import state from './state'
 import mutations from './mutations'
+import actions from './actions'
+import getters from './getters'
 const debug = process.env.NODE_ENV !== 'production'
 
 export default createStore({
   strict: debug,
   plugins: debug ? [createLogger()] : [],
   state: state,
-  getters: {},
-  actions: {},
+  getters: getters,
+  actions: actions,
   mutations: mutations
 })
