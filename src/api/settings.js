bb91674cd13b42a62216c1d9b7a907cc13fa09ac
VoidConcept <carlos@estudioseijo.com>
2021-06-20 17:23:00 +0200

    fix: deleting settings.js from gitignore to first deploy in vercel

---
+++ src/api/settings.js
@@ -0,0 +1,11 @@
+// FireBASE Config
+export default {
+  apiKey: 'AIzaSyCjtoP4KMOkcTCwX3dFPcUOKBuAo59B6Fo',
+  authDomain: 'vuemyco.firebaseapp.com',
+  databaseURL: 'https://vuemyco-default-rtdb.europe-west1.firebasedatabase.app',
+  projectId: 'vuemyco',
+  storageBucket: 'vuemyco.appspot.com',
+  messagingSenderId: '675319384263',
+  appId: '1:675319384263:web:bd3ad525e9e97e928616e2',
+  measurementId: 'G-GBQ453C2QC'
+}
